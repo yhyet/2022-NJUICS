c26f6a9ad72cbfee2992f3ba24faa24242cdb4fe
tracer-ics2022 <tracer@njuics.org>
2022-10-05 21:00:50 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:00:50 up 55 min,  1 user,  load average: 0.48, 0.32, 0.22

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -48,7 +48,8 @@ static struct rule {
   {"\\)",')'},
   {"0x[0-9,a-f]+",TK_HEXNUM},
   {"[0-9]+",TK_NUMBER},
-  {"\\$[$pcrsgta0-9]{2,3}",TK_REGNAME},
+  //{"\\$[$pcrsgta0-9]{2,3}",TK_REGNAME},
+  {"\\$(\\$0|ra|sp|gp|tp|t0|t1|t2|s0|s1|a0|a1|a2|a3|a4|a5|a6|a7|s2|s3|s4|s5|s6|s7|s8|s9|s10|s11|t3|t4|t5|t6|pc)",TK_REGNAME},
   {"!=",TK_NOTEQ},
   {"&&",TK_AND},
 
