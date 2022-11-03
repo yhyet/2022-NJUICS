837b6594a4b998356fead7eaf24cb3364b895805
tracer-ics2022 <tracer@njuics.org>
2022-11-04 01:15:08 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:15:08 up  2:23,  1 user,  load average: 0.24, 0.16, 0.16

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,30 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int reverse(char *a,int len){
+  int i=0;
+  int t;
+  for ( i = 0; i < len/2; i++)
+  {
+    t=a[i];
+    a[i]=a[len-1-i];
+    a[len-1-i]=t;
+  }
+  return 0;
+}
+void inttostring(int b,char *c){
+  char temp[2]={0};
+  while (b%10)
+  {
+    char a=b%10+'0';
+    temp[0]=a;
+    b=b/10;
+    strcat(c,temp);
+  }
+  reverse(c,strlen(c));
+
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +38,51 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  va_list ap;
+  va_start (ap,fmt);
+  while (*fmt!= '\0')
+  {
+    if (*fmt!='%')
+    {
+      *out=*fmt;
+      fmt++;
+      out++;
+      continue;
+    }
+    fmt++;
+    switch (*fmt++)
+    {
+    case 's':
+      char *s;
+      s=va_arg(ap,char *);
+      while (*s)
+      {
+        *out=*s;
+        out++;
+        s++;
+      }
+      break;
+
+    case 'd':
+      int d;
+      d=va_arg(ap,int);
+      char s1[128]={0};
+      char *p=s1;
+      //char *s1;
+      inttostring(d,p);
+      while (*p)
+      {
+        *out=*p;
+        out++;
+        p++;
+      }
+      break;
+    default:
+      break;
+    }
+  }
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
