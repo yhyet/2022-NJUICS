0c92eb83b72b1c0fb7f4cfcd40c4ab4a1ff95b35
tracer-ics2022 <tracer@njuics.org>
2022-11-10 17:34:01 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:34:01 up  4:20,  1 user,  load average: 0.76, 0.36, 0.26

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -18,6 +18,8 @@
 #include <device/mmio.h>
 #include <isa.h>
 
+static void mtrace(paddr_t addr,int len,word_t data,int flag);
+
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
@@ -57,14 +59,40 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  if (likely(in_pmem(addr))){
+    #ifdef CONFIG_MEMORY_TRACE
+      if(CONFIG_MEMORY_TRACE)
+        mtrace(addr,len,-1,1);
+    #endif
+    return pmem_read(addr, len);
+  }
+  //IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_pmem(addr))) { 
+    pmem_write(addr, len, data);
+    #ifdef CONFIG_MEMORY_TRACE
+      if(CONFIG_MEMORY_TRACE)
+        mtrace(addr,len,data,0);
+    #endif
+    //IFDEF(CONFIG_MEMORY_TRACE, mtrace(addr,len,data,0));
+    return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
+}
+
+static void mtrace(paddr_t addr,int len,word_t data,int flag){
+  if (flag==1)
+  {
+    log_write("men_read : ");
+    log_write("%i",pmem_read(addr,len));
+  }
+  else if (flag==0)
+  {
+    log_write("men_write: ");
+    log_write("%i",data);
+  }
 }
