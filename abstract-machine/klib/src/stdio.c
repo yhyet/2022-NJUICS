ac2f5d9c443a5a8aaeb230313a85f275ca051219
tracer-ics2022 <tracer@njuics.org>
2022-11-16 18:41:41 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:41:41 up  8:29,  1 user,  load average: 0.39, 0.22, 0.13

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -15,80 +15,8 @@ void int2char(int d,int digit,char *a){
   }
 }
 
-int printf(const char *fmt, ...) {
-  //panic("Not implemented");
-  va_list ap;
-  va_start (ap,fmt);
-  int d=0;
-  char *s;
-  while (*fmt!= '\0')
-  {
-    if (*fmt!='%')
-    {
-      putch(*fmt);
-      fmt++;
-      continue;
-    }
-    fmt++;
-    switch (*fmt)
-    {
-    case 's':{
-      s=va_arg(ap,char *);
-      while (*s!='\0')
-      {
-        putch(*s);
-        s++;
-      }
-      fmt++;
-      break;}
-
-    case 'd':{
-      d=va_arg(ap,int);
-      int digit=0,d1=d;
-      do{
-        d1=d1/10;
-        digit++;
-      }
-      while (d1>0);
-      char a[128]={};
-      //char *putchp=
-      int2char(d,digit,a);
-      /*
-      int i=0;
-      while (a[i]!='\0')
-      {
-        putch(a[i]);
-        i++;
-      }*/
-      for (int i = 0; i < digit; i++)
-      {
-        putch(a[i]);
-      }
-      
-      
-      fmt++;
-      break;}
-    default:{
-      putch(*fmt);
-      fmt++;
-      break;}
-    }
-    //fmt++;
-  }
-  va_end(ap);
-  //out++;
-  putch('\0');
-  return 0;
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start (ap,fmt);
-  int d=0;
+int makestr(char *out,const char *fmt,va_list ap){
+    int d=0;
   char *s;
   while (*fmt!= '\0')
   {
@@ -156,6 +84,81 @@ int sprintf(char *out, const char *fmt, ...) {
   }
   while(*out!='\0');
   return n;
+}
+
+int printf(const char *fmt, ...) {
+  //panic("Not implemented");
+  va_list ap;
+  va_start (ap,fmt);
+  int d=0;
+  char *s;
+  while (*fmt!= '\0')
+  {
+    if (*fmt!='%')
+    {
+      putch(*fmt);
+      fmt++;
+      continue;
+    }
+    fmt++;
+    switch (*fmt)
+    {
+    case 's':{
+      s=va_arg(ap,char *);
+      while (*s!='\0')
+      {
+        putch(*s);
+        s++;
+      }
+      fmt++;
+      break;}
+
+    case 'd':{
+      d=va_arg(ap,int);
+      int digit=0,d1=d;
+      do{
+        d1=d1/10;
+        digit++;
+      }
+      while (d1>0);
+      char a[128]={};
+      int2char(d,digit,a);
+      /*
+      int i=0;
+      while (a[i]!='\0')
+      {
+        putch(a[i]);
+        i++;
+      }*/
+      for (int i = 0; i < digit; i++)
+      {
+        putch(a[i]);
+      }
+      
+      
+      fmt++;
+      break;}
+    default:{
+      putch(*fmt);
+      fmt++;
+      break;}
+    }
+    //fmt++;
+  }
+  va_end(ap);
+  //out++;
+  putch('\0');
+  return 0;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  panic("Not implemented");
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start (ap,fmt);
+  return makestr(out,fmt,ap);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
