5f55686db6de38172b52a7138d0345d52a2c4f74
tracer-ics2022 <tracer@njuics.org>
2022-11-17 14:47:19 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:47:19 up  1:49,  1 user,  load average: 1.23, 0.91, 0.88

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -10,5 +10,9 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   //int k=
   kbd->keydown = (inl(KBD_ADDR) & KEYDOWN_MASK ? true : false);
   kbd->keycode = inl(KBD_ADDR) & ~KEYDOWN_MASK;
-
+  if (kbd->keydown)
+  {
+    printf("true");
+  }
+  
 }
