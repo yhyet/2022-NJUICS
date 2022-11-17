68c23bce9b45599e951cd91b5589a124eb972952
tracer-ics2022 <tracer@njuics.org>
2022-11-17 15:05:12 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:05:12 up  2:06,  1 user,  load average: 0.93, 0.97, 0.93

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,12 +3,14 @@
 #include <klib.h>
 #define KEYDOWN_MASK 0x8000
 #define KEY_QUEUE_LEN 1024
+//static int keymap[256] = {};
 //static int key_queue[KEY_QUEUE_LEN] = {};
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   //printf("yhy-%d\n",inl(KBD_ADDR));
   //int k=
-  kbd->keydown = (inl(KBD_ADDR) & KEYDOWN_MASK ? true : false);
+  //kbd->keydown = (inl(KBD_ADDR) & KEYDOWN_MASK ? true : false);
+  kbd->keydown=1;
   kbd->keycode = inl(KBD_ADDR) & ~KEYDOWN_MASK;
   if (kbd->keydown==0)
   {
