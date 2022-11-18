696ceb4067866f5cdabdecf2238140ae2f756532
tracer-ics2022 <tracer@njuics.org>
2022-11-19 01:58:19 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:58:19 up 11 min,  1 user,  load average: 0.20, 0.28, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,9 +1,20 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <klib.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define W 400
+#define H 300
 
 void __am_gpu_init() {
+  int i;
+  int w=0;
+  int h=0;
+  uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
+  for ( i = 0; i < w*h; i++)
+  {
+    fb[i]=i;
+  }
+  outl(SYNC_ADDR,1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -18,6 +29,18 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
+  int i = 0;
+  int j = 0;
+  //ctl->h = H;
+  //ctl->w = W;
+  for(i = 0;i <ctl->h;i++){
+    for(j = 0;j <ctl->w;j++){
+        outl(FB_ADDR + i*W*4 + j*4,*(uint32_t *)(ctl->pixels));
+        (ctl->pixels)+=4;
+        
+    }
+  }
+  //printf("%d,%d\n",ctl->h,ctl->w);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -15,7 +15,7 @@
 
 #include <common.h>
 #include <device/map.h>
-
+#include <memory/paddr.h>
 #define SCREEN_W (MUXDEF(CONFIG_VGA_SIZE_800x600, 800, 400))
 #define SCREEN_H (MUXDEF(CONFIG_VGA_SIZE_800x600, 600, 300))
 
@@ -71,6 +71,12 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
+    if(paddr_read(CONFIG_VGA_CTL_MMIO+4,4)!=0){
+        update_screen();
+    }
+    //printf("%08x\n",paddr_read(CONFIG_VGA_CTL_MMIO+4,4));
+    paddr_write(CONFIG_VGA_CTL_MMIO+4,4,0);
+    
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
 }
