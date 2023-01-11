1dfc71b14f07aff00b28a53b99674674eb10a6f3
tracer-ics2022 <tracer@njuics.org>
2023-01-12 04:10:45 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:10:45 up 15:07,  1 user,  load average: 0.60, 0.57, 0.48

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -1,9 +1,10 @@
 #include <am.h>
 #include <riscv/riscv.h>
 #include <klib.h>
+#include <arch/riscv32-nemu.h>
 
 static Context* (*user_handler)(Event, Context*) = NULL;
-
+/*
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
@@ -17,6 +18,34 @@ Context* __am_irq_handle(Context *c) {
 
   return c;
 }
+*/
+
+Context* __am_irq_handle(Context *c) {
+  if (user_handler) {
+    Event ev = {0};
+    
+    switch (c->mcause) {
+      case (11):
+        // printf("c->GPR1 = %d \n", c->GPR1);
+        if (c->GPR1 == -1){ // ÌØÖ¸-1
+          ev.event = EVENT_YIELD;
+        }else {
+          ev.event = EVENT_SYSCALL;
+        }
+        break;
+
+      default: ev.event = EVENT_ERROR; break;
+    }
+
+    c = user_handler(ev, c);
+    assert(c != NULL);
+    c->mepc += 4;
+  }
+
+  return c;
+}
+
+
 
 extern void __am_asm_trap(void);
 
