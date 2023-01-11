2f3c71edd43da885d3f4d746264b91ad108755c3
tracer-ics2022 <tracer@njuics.org>
2023-01-12 04:22:52 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:22:52 up 15:19,  1 user,  load average: 0.62, 0.40, 0.41

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -146,9 +146,9 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, (s->dnpc)=(s->pc)+(imm); R(dest)=s->pc+4);
 
   //TYPE_N
-  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, printf("reach ebreak!\n");NEMUTRAP(s->pc, R(10))); // R(10) is $a0
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall , N, printf("reach ecall!\n");(s->dnpc)=isa_raise_intr(11,s->pc);printf("dnpc=%#x\n",s->dnpc););
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret , N, printf("reach mret!\n");(s->dnpc)=(cpu.mepc));
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall , N, (s->dnpc)=isa_raise_intr(11,s->pc);printf("dnpc=%#x\n",s->dnpc););
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret , N, printf("mepc=%#x\n",cpu.mepc);(s->dnpc)=(cpu.mepc));
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
