b011c968b46e30350a4a5052570f924c7294adf5
tracer-ics2022 <tracer@njuics.org>
2023-01-12 03:05:45 +0800

    >  compile NEMU 211300010 易慧宇 Linux yhyet-Lenovo-Legion-Y7000P2021 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:05:45 up 14:02,  1 user,  load average: 0.45, 0.43, 0.38

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -9,8 +9,8 @@ void init_proc(void);
 
 int main() {
   extern const char logo[];
+  printf("hello world--yhy\n");
   printf("%s", logo);
-  printf("hello world--yhy");
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
